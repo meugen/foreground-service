fad183c0fec1cf4c411c1d0a9b1235db44dfd527
meugen <meugen@ua.fm>
2018-07-13 23:16:27 +0300

    Fixed compiling

--- app/src/main/java/meugeninua/foregroundservice/ui/activities/main/MainActivity.java
+++ app/src/main/java/meugeninua/foregroundservice/ui/activities/main/MainActivity.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 
 import meugeninua.foregroundservice.R;
 import meugeninua.foregroundservice.ui.activities.base.BaseActivity;
+import meugeninua.foregroundservice.ui.activities.base.fragments.base.binding.Binding;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity<Binding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
